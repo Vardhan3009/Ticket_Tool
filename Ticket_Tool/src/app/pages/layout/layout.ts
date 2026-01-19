fd45cb1f501bc55ebdef3131fd39af2fe8bdb10d
Vardhan <harsh.vardhan@siemens-healthineers.com>
2026-01-20 00:54:15 +0530

    LogOut functionality added

--- Ticket_Tool/src/app/pages/layout/layout.ts
+++ Ticket_Tool/src/app/pages/layout/layout.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
 
+  router=inject(Router);
+
+  onLogOut(){
+    localStorage.removeItem('ticketUser');
+    this.router.navigateByUrl('/login');
+  }
 }
